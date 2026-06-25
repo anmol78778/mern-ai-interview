ed5b23bf65cce85b588b9399b5482953d2cc2e5e
Anmol Kumar <anmolkroe@gmail.com>
2026-06-26 02:40:06 +0530

    feat: implement user authentication with Google sign-in and JWT token management

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,29 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDb from "./config/connectDb.js"
-const app=express()
+import authRouter from "./routes/auth.route.js"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import userRouter from "./routes/user.route.js"
 
+const app=express()
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 dotenv.config()
+
+
+
+app.use(express.json())
+app.use(cookieParser())
+
 const PORT=process.env.PORT
 
 
-app.get('/',(req,res)=>{
-    return res.json({message:"server started"})
-})
-
+app.use('/api/auth',authRouter)
+app.use('/api/user',userRouter)
 
 
 const mongo=process.env.MONGODB_URL
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,28 @@ import Home from './pages/Home'
 import Auth from './pages/auth'
 import { useEffect } from 'react'
 import axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { setUserData } from './redux/userSlice'
+
+export const ServerUrl  = "http://localhost:8000"
 
 function App() {
+ const dispatch = useDispatch()
+  useEffect(()=>{
+    const getUser = async () => {
+      try {
+        const result = await axios.get(ServerUrl + "/api/user/current-user", {withCredentials:true})
+        dispatch(setUserData(result.data))
+        console.log(result)
+      } catch (error) {
+        console.log(error)
+        dispatch(setUserData(null))
+      }
+    }
+    getUser()
+
+  },[dispatch])
+
   return (
     <div>
       <Routes>
