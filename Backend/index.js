2ea15849ce046e78f0d6e670a3b6a93f5d413503
Anmol Kumar <anmolkroe@gmail.com>
2026-06-25 23:32:22 +0530

    initial commit

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./config/connectDb.js"
+const app=express()
+
+
+dotenv.config()
+const PORT=process.env.PORT
+
+
+app.get('/',(req,res)=>{
+    return res.json({message:"server started"})
+})
+
+const mongo=process.env.MONGODB_URL
+app.listen(PORT,()=>{
+    connectDb()
+    console.log(`server running on port ${PORT} `)
+})
